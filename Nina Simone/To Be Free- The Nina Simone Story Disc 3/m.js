954d91ac49450434c9c3e9065f51669a02e25d2a
littleflute <13699175164@sina.cn>
2017-07-27 17:21:52 -0500

    Add files via upload

---
+++ Nina Simone/To Be Free- The Nina Simone Story Disc 3/m.js
@@ -0,0 +1,16 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 No Opportunity Necessary, No Experience Needed [_][Live].mp3";		
+f[2]	= "02 Just Like a Woman.mp3";		
+f[3]	= "03 Here Comes the Sun.mp3";		
+f[4]	= "04 Tanywey [_].mp3";		
+f[5]	= "05 Funkier Than a Mosquito's Tweeter [Live].mp3";		
+f[6]	= "06 My Sweet Lord-Today Is a Killer [Live][Medley].mp3";		
+f[7]	= "07 Let It Be Me [_][Live].mp3";		
+f[8]	= "09 Mr. Bojangles [Live].mp3";		
+f[9]	= "10 I Want a Little Sugar in My Bowl [Live].mp3";		
+f[10]	= "11 Nina [_][Live].mp3";		
+f[11]	= "12 Zungo [_][Live].mp3";		
+f[12]	= "14 A Single Woman.mp3";		
+f[13]	= "Baltimore.mp3";		
+f[14]	= "Poppies.mp3";		
