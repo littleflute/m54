970c050adc8ba00be338ce6311f3908f4fe91bc1
littleflute <13699175164@sina.cn>
2017-07-27 17:21:50 -0500

    Add files via upload

---
+++ Logic/Everybody/m.js
@@ -0,0 +1,15 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "04 Killing Spree.mp3";		
+f[2]	= "05 Take It Back.mp3";		
+f[3]	= "07 Ink Blot.mp3";		
+f[4]	= "08 Mos Definitely.mp3";		
+f[5]	= "09 Waiting Room.mp3";		
+f[6]	= "1-800-273-8255.mp3";		
+f[7]	= "12 Black SpiderMan.mp3";		
+f[8]	= "AfricAryaN.mp3";		
+f[9]	= "America.mp3";		
+f[10]	= "Anziety.mp3";		
+f[11]	= "Confess.mp3";		
+f[12]	= "Everybody.mp3";		
+f[13]	= "Hallelujah.mp3";		
